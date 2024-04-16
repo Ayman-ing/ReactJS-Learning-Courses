522ff7d2cfab6adc294a8b0e88c4df473a2e1f8f
Ayman-ing <aymanfeki1@gmail.com>
2024-04-16 18:56:13 +0100

    adding the 404 notFound page

--- react-jobs/src/App.jsx
+++ react-jobs/src/App.jsx
@@ -3,11 +3,13 @@ import {Route,createBrowserRouter,createRoutesFromElements,RouterProvider} from 
 import HomePage from './pages/HomePage.jsx';
 import MainLayout  from './layouts/MainLayout.jsx';
 import JobsPage from './pages/JobsPage.jsx';
+import NotFoundPage from './pages/NotFoundPage.jsx';
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path="/" element={<MainLayout/>}>
   <Route index element={<HomePage/>} />
   <Route path="/jobs" element={<JobsPage/>} />
+  <Route path="*" element={<NotFoundPage/>} />
   </Route>
   ));
 
